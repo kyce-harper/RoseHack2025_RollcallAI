8d73677fb5385ec5705444093e79a8908db5a94c
James Mace <macewjames@gmail.com>
2025-01-25 22:46:32 -0800

    adding comments to pydataend

--- pydataend/main.py
+++ pydataend/main.py
@@ -1,6 +1,8 @@
-class Class:
+class Class: # class that a student is enrolled in
     def __init__(self):
+        #class has a name (ex: math011)
         self.class_name = ""
+        #and a time (ex: MW, 3-5)
         self.class_time = 0
 
     def set_time(self, time):
@@ -16,8 +18,10 @@ class Class:
         return self.class_name
 
 
-class User:
+class User: #base class for users, classes student and employee inherit User
     def __init__(self, key=-1, name="noName"):
+        #all users, students or employees have a name and a key 
+        #key is the user's unqiue ID, it is also their position in the array of users
         self._name = name
         self._key = key
 
@@ -38,9 +42,13 @@ class User:
 class Student(User):
     def __init__(self, key=-1, name="noName", grade=-1, major="noMajor", num_classes=0):
         super().__init__(key, name)
+        #students have a grade (ex: sophomore)
         self._grade = grade
+        #students have a major (computer science)
         self._major = major
+        #students have a number of classes
         self._num_classes = num_classes
+        #and an array of class objects, aka the students schedule
         self._classes = []
 
     def set_grade(self, grade):
@@ -64,10 +72,15 @@ class Student(User):
 class Employee(User):
     def __init__(self, key=-1, name="noName", tenure=0, job="noJob", work_days="noWorkDays", start_time=-1, end_time=-1):
         super().__init__(key, name)
+        #employeers have tenure (ex: 4 )
         self._tenure = tenure
+        #and a job (ex: professor)
         self._job = job
+        #and work days (ex: MTW)
         self._work_days = work_days
+        #start time ex: 10am
         self._start_time = start_time
+        #and an end time ex: 7 pm
         self._end_time = end_time
 
     def set_tenure(self, tenure):
@@ -90,7 +103,7 @@ class Employee(User):
         print(f"{self._start_time} -- {self._end_time}")
 
 
-def read_user(file_name):
+def read_user(file_name): #read users in from input
     try:
         with open(file_name, 'r') as file:
             key = int(file.readline().strip())
